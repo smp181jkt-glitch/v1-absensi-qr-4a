b83fd9d269f26e4908a74605d07b4d05ef6d2e2c
smp181jkt-glitch <smp181jkt@gmail.com>
2026-07-15 00:21:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const WEBAPP = "https://script.google.com/macros/s/AKfycbzScCPxNEXGGyo1X0ArFx_WYYzYgu5qLowcRo7TNmrmbKH3gNVEWWKzcUFrWcLKPTHr/exec";
+const WEBAPP = "https://script.google.com/macros/s/AKfycbza74zz7TsOx72fug7f8Jph-o1Q19a8j8AOPtvffGvtpps1hJFzKykI5FAZwM1L7R6t/exec";
 
 const hasil = document.getElementById("hasil");
 const tombol = document.getElementById("scanBtn");
